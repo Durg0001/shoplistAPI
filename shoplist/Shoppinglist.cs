76b4fddaa339bdd5ca0e50aedec8f2635a154124
Durga Prasad Mishra <durg0001@edu.zealand.dk>
2023-03-28 10:41:39 +0200

    Add project files.

---
+++ shoplist/Shoppinglist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoplistAPI 
+{
+    public class Shoppinglist
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+        public override string ToString()
+        {
+            return id + name + price + quantity;
+
+        }
+    }
+
+}
